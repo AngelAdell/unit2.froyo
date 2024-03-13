9e43654caf9651b4a1eed1c8c2695f3373583afb
Angel Adell <angeldeniseadell@gmail.com>
2024-03-13 17:51:31 -0400

    completed project with Dom content

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const Froyo = [ "Vanilla", "Chocolate", "Strawberry", "Rocky Road"]
 console.log(Froyo);
+const addedFlavors = [];
 
 const buttons = document.querySelectorAll(".addButton");
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function() {
       console.log("You have added " + Froyo[i] + " to your order.");
+      addedFlavors.push(Froyo[i]);
+      console.log(addedFlavors);
     });
   }
 
 const startOverButton = document.querySelector(".startOverButton");
    startOverButton.addEventListener("click", function() {
     if (confirm("Are you sure you want to start over?")) {
+      console.clear();
       console.log("You have started over.");
     } 
     else {  console.log("Continue Order."); 
